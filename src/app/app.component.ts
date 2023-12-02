54d38d4808b7dec25d4adc5eadf49c17f05e2873
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2023-12-02 12:04:55 -0500

    feat: agregar componente usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'IlanAngularTest';
+  title = 'Mi primer proyecto en Angular 17';
+  city = 'Moro';
 }
