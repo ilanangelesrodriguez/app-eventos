1136ad0172fb8ddcc7e7b025a897bf2a06e7d84f
ilanangelesrodriguez <ilanangelesrodriguez@gmail.com>
2024-08-16 04:33:01 +0000

    feat: agregar componente form

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,4 @@
+import Form from "@/components/form/form";
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 
@@ -7,6 +8,9 @@ export default function DocsPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>About</h1>
+
+          <Form />
+
         </div>
       </section>
     </DefaultLayout>
